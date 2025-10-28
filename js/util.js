d0c10d435eb2d1893dbd6e767d96ff9b8eb65e49
Ofek Meshulam <ofekmeshulam46@gmail.com>
2025-10-28 18:35:19 +0200

    getrandomcolor add

--- js/util.js
+++ js/util.js
@@ -1,10 +1,17 @@
 'use strict'
 
-
 function getRandomIntExclusive(min, max) {
   const minCeiled = Math.ceil(min)
   const maxFloored = Math.floor(max)
   return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)
 }
 
+function getRandomColor() {
+  const letters = '0123456789ABCDEF'
+  var color = '#'
 
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)]
+  }
+  return color
+}
