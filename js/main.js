49bba5af392c7b69d7b77f8ec3b5f970de3d9ac9
Ofek Meshulam <ofekmeshulam46@gmail.com>
2025-10-28 16:00:09 +0200

    onBallClick increment added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 'use strict'
 
-function onBallClick(ball){
- ball.style.width += 50
+const elBallContent = document.querySelector('.ball')
+
+function onBallClick(ball) {
+  const currBallWidth = ball.offsetWidth
+  const currBallHeight = ball.offsetHeight
+  const increment = 50
+  const newBallWidth = currBallWidth + increment
+  const newBallHeight = currBallHeight + increment
+
+  ball.style.width = newBallWidth + 'px'
+  ball.style.height = newBallHeight + 'px'
+
+  elBallContent.innerHTML=newBallHeight
+
 }
